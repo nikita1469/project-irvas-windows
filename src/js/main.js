4c195fde1af4e97bf7f16a0ba8b1090ce3bceb1c
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-06-28 23:22:30 +0300

    second commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import './slider';
+import modals from './modules/modals';
+import tabs from './modules/tabs';
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+    tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+});
+
+// console.log(1);
